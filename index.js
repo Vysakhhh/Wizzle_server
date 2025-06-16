ddddd133bb058fb9783abae6402ee302a1ff5b4b
Vysakhhh <tharammelvysakh@gmail.com>
2025-06-17 00:36:22 +0530

    Fix CORS

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const express=require('express')
-const cors =require('cors')
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
-const userRoutes=require('./routes/userRoutes')
-const adminRoutes=require('./routes/adminRoutes')
+const userRoutes = require('./routes/userRoutes')
+const adminRoutes = require('./routes/adminRoutes')
 require('./db/db_connection')
-const cookieParser=require('cookie-parser')
+const cookieParser = require('cookie-parser')
 const { wServer, server } = require('./lib/socket');
 
 
 wServer.use(express.json())
 wServer.use(cookieParser())
 wServer.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-   
+    origin: ["http://localhost:5173",
+        "https://wizzle-frontend.vercel.app"],
+
+    credentials: true,
+
 }))
 
 wServer.use(userRoutes)
 wServer.use(adminRoutes)
-const PORT=process.env.PORT ||  3000
+const PORT = process.env.PORT || 3000
 
-server.listen(PORT,()=>{
+server.listen(PORT, () => {
     console.log(`wServer started running at port ${PORT}`);
-    
+
 
 })
 
-wServer.get('/',(req,res)=>{
+wServer.get('/', (req, res) => {
     console.log(`wServer started running at port ${PORT} and waiting for client request`);
-    
+
 })
