f3bc37c7b4ca5648b98ac9e8bf065b0e447e8e7e
Vysakhhh <tharammelvysakh@gmail.com>
2025-06-18 21:46:56 +0530

    userController update

--- controller/userController.js
+++ controller/userController.js
@@ -25,7 +25,7 @@ exports.signUpController = async (req, res) => {
 
     await newUser.save();
 
-    const token = generateToken(newUser);  
+    const token = generateToken(newUser);
 
     return res.status(201).json({
       token,
@@ -34,7 +34,7 @@ exports.signUpController = async (req, res) => {
         fullName: newUser.fullName,
         email: newUser.email,
         role: newUser.role,
-        profilePic:"" 
+        profilePic: ""
       },
     });
 
@@ -48,41 +48,41 @@ exports.signUpController = async (req, res) => {
 exports.logInController = async (req, res) => {
   const { email, password } = req.body;
 
- try{
-     const existingUser = await users.findOne({ email });
+  try {
+    const existingUser = await users.findOne({ email });
 
-  if (!existingUser) {
-    return res.status(401).json('Invalid credentials');
+    if (!existingUser) {
+      return res.status(401).json('Invalid credentials');
+    }
+
+    const isUserPswdMatch = await bcrypt.compare(password, existingUser.password);
+
+
+    if (isUserPswdMatch || password === existingUser.password) {
+
+      const token = generateToken(existingUser);
+
+      res.status(200).json({
+        token,
+        user: {
+          _id: existingUser._id,
+          fullName: existingUser.fullName,
+          email: existingUser.email,
+          role: existingUser.role,
+          profilePic: existingUser.profilePic || ""
+        },
+      });
+    }
+    else {
+      res.status(409).json("Invalid credentials")
+    }
+
   }
-
-  const isUserPswdMatch = await bcrypt.compare(password, existingUser.password);
- 
-
-  if(isUserPswdMatch || password ===existingUser.password){
-    
-  const token = generateToken(existingUser);
-
-  res.status(200).json({
-    token,
-    user: {
-      _id: existingUser._id,
-      fullName: existingUser.fullName,
-      email: existingUser.email,
-      role: existingUser.role,
-      profilePic:" "
-    },
-  });
-  }
-  else{
-    res.status(409).json("Invalid credentials")
-  }
-
- }
- catch(err){
-    console.log("Error in loginInController",err);
+  catch (err) {
+    console.log("Error in loginInController", err);
     res.status(500).json("Internal server error")
-    
- }
+
+  }
 };
 
 exports.logOutController = async (req, res) => {
@@ -97,45 +97,45 @@ exports.logOutController = async (req, res) => {
 
 
 exports.updateProfileController = async (req, res) => {
-    console.log("inside updateProfileController");
+  console.log("inside updateProfileController");
 
-    const { profilePic } = req.body
-    const userId = req.user._id
+  const { profilePic } = req.body
+  const userId = req.user._id
 
-    try {
+  try {
 
-        if (!profilePic) {
-            return res.status(400).json("profilePic is required")
-        }
-
-        const uploadResponse = await cloudinary.uploader.upload(profilePic)
-        const updatedUser = await users.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
-
-        res.status(200).json(updatedUser)
-
+    if (!profilePic) {
+      return res.status(400).json("profilePic is required")
     }
-    catch (err) {
-        console.log("Error in updateProfileController", err.message);
-        res.status(500).json("Internal server error")
+
+    const uploadResponse = await cloudinary.uploader.upload(profilePic)
+    const updatedUser = await users.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, { new: true })
+
+    res.status(200).json(updatedUser)
+
+  }
+  catch (err) {
+    console.log("Error in updateProfileController", err.message);
+    res.status(500).json("Internal server error")
 
 
-    }
+  }
 
 }
 
 exports.checkAuthentication = async (req, res) => {
 
-    console.log("inside checkAuthentication");
+  console.log("inside checkAuthentication");
 
-    const user=req.user
+  const user = req.user
 
-    try {
-        res.status(200).json(user)
-    }
-    catch (err) {
-        console.log("Error in checkAuthentication", err.message);
-        res.status(500).json("Internal server error")
+  try {
+    res.status(200).json(user)
+  }
+  catch (err) {
+    console.log("Error in checkAuthentication", err.message);
+    res.status(500).json("Internal server error")
 
-    }
+  }
 
 }
